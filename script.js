fc1f64f25cfd65a71040b1c4a5080bc0f29efda4
gabdykalyk <gabdykalyk@gmail.com>
2020-07-04 20:50:50 +0600

    fixed

--- script.js
+++ script.js
@@ -1,157 +1,157 @@
-// TASK 1
-function week() {
-    let dayNumber;
-    dayNumber = prompt("Какой сегодня по счёту день?");
-    switch (dayNumber) {
-        case '1':
-            dayName = "Понедельник";
-            break;
-        case '2':
-            dayName = "Вторник";
-            break;
-        case '3':
-            dayName = "Среду";
-            break;
-        case '4':
-            dayName = "Четверг";
-            break;
-        case '5':
-            dayName = "Пятница";
-            break;
-        case '6':
-            dayName = "Суббота";
-            break;
-        case '7':
-            dayName = "Воскресенье";
-            break;
-        default:
-            dayName = "Неизвестный день недели";
-    }
-    return dayName;
-}
+// // TASK 1
+// function week() {
+//     let dayNumber;
+//     dayNumber = prompt("Какой сегодня по счёту день?");
+//     switch (dayNumber) {
+//         case '1':
+//             dayName = "Понедельник";
+//             break;
+//         case '2':
+//             dayName = "Вторник";
+//             break;
+//         case '3':
+//             dayName = "Среду";
+//             break;
+//         case '4':
+//             dayName = "Четверг";
+//             break;
+//         case '5':
+//             dayName = "Пятница";
+//             break;
+//         case '6':
+//             dayName = "Суббота";
+//             break;
+//         case '7':
+//             dayName = "Воскресенье";
+//             break;
+//         default:
+//             dayName = "Неизвестный день недели";
+//     }
+//     return dayName;
+// }
 
-console.log(week());
+// console.log(week());
 
 
-// TASK2
-function evenOdd() {
-    let result;
-    let number = prompt('Введите число');
-    if (Number.isInteger(Number(number))) {
-        if (number % 2 === 0) {
-            result = `${number} чётное число`;
-        } else {
-            result = `${number} нечётное число`
-        }
-    } else {
-        console.log(`Ошибка, ${number} не является целым числом`)
-    }
-    console.log(result)
-}
+// // TASK2
+// function evenOdd() {
+//     let result;
+//     let number = prompt('Введите число');
+//     if (Number.isInteger(Number(number))) {
+//         if (number % 2 === 0) {
+//             result = `${number} чётное число`;
+//         } else {
+//             result = `${number} нечётное число`
+//         }
+//     } else {
+//         console.log(`Ошибка, ${number} не является целым числом`)
+//     }
+//     console.log(result)
+// }
 
-evenOdd()
+// evenOdd()
 
-// TASK 3
-function power() {
-    let a = prompt("Введите число")
-    let b = prompt("В какую степень вы бы хотели его возвести?")
-    let result;
-    if (typeof Number(a, b) === 'number' && Number.isInteger(Number(b)) && !Number.isNaN(Number(a))) {
-        result = a ** b;
-    } else {
-        result = "Вы ввели неверные данные";
-    }
-    console.log(result)
-}
+// // TASK 3
+// function power() {
+//     let a = prompt("Введите число")
+//     let b = prompt("В какую степень вы бы хотели его возвести?")
+//     let result;
+//     if (typeof Number(a, b) === 'number' && Number.isInteger(Number(b)) && !Number.isNaN(Number(a))) {
+//         result = a ** b;
+//     } else {
+//         result = "Вы ввели неверные данные";
+//     }
+//     console.log(result)
+// }
 
-power()
+// power()
 
-// TASK 4
-function canDivide() {
-    let result;
-    let a = prompt('Введите первое число');
-    let b = prompt("Введите второе число")
-    if (typeof Number(a, b) === 'number' && !Number.isNaN(Number(a)) && !Number.isNaN(Number(b)) && Number.isFinite(a / b)) {
-        if (a % b === 0) {
-            result = `${a} делится на ${b}, получаем ${a / b}`;
-        } else {
-            result = `${a} делится на ${b} с остатком`
-        }
-    } else {
-        console.log(`Ошибка, неверные данные`)
-    }
-    console.log(result)
-}
+// // TASK 4
+// function canDivide() {
+//     let result;
+//     let a = prompt('Введите первое число');
+//     let b = prompt("Введите второе число")
+//     if (typeof Number(a, b) === 'number' && !Number.isNaN(Number(a)) && !Number.isNaN(Number(b)) && Number.isFinite(a / b)) {
+//         if (a % b === 0) {
+//             result = `${a} делится на ${b}, получаем ${a / b}`;
+//         } else {
+//             result = `${a} делится на ${b} с остатком`
+//         }
+//     } else {
+//         console.log(`Ошибка, неверные данные`)
+//     }
+//     console.log(result)
+// }
 
-canDivide()
+// canDivide()
 
-// TASK 5
-function random() {
-    let a = Number(prompt("Введите первое число"));
-    let b = Number(prompt("Введите второе число"));
-    let min;
-    let max;
-    let result;
-    function RandomNumber() {
-        result = min + Math.random() * (max - min);
-        console.log(Math.round(result))
-    }
-    if (typeof (a, b) === 'number' && !Number.isNaN(a) && !Number.isNaN(b)) {
-        if (a < b) {
-            min = a;
-            max = b;
-            RandomNumber()
-        } else if (a > b) {
-            min = b;
-            max = a;
-            RandomNumber()
-        } else {
-            console.log("Числа равны")
-        }
-    } else {
-        console.log("Ошибка, вы не ввели два числа")
-    }
+// // TASK 5
+// function random() {
+//     let a = Number(prompt("Введите первое число"));
+//     let b = Number(prompt("Введите второе число"));
+//     let min;
+//     let max;
+//     let result;
+//     function RandomNumber() {
+//         result = min + Math.random() * (max - min);
+//         console.log(Math.round(result))
+//     }
+//     if (typeof (a, b) === 'number' && !Number.isNaN(a) && !Number.isNaN(b)) {
+//         if (a < b) {
+//             min = a;
+//             max = b;
+//             RandomNumber()
+//         } else if (a > b) {
+//             min = b;
+//             max = a;
+//             RandomNumber()
+//         } else {
+//             console.log("Числа равны")
+//         }
+//     } else {
+//         console.log("Ошибка, вы не ввели два числа")
+//     }
 
-}
+// }
 
-random()
+// random()
 
-// TASK6
-function isYearLeap() {
-    let year = Number(prompt("Введите год"));
-    let result;
-    if (Number.isInteger(year)) {
-        if ((year % 4 === 0 && !(year % 100 === 0)) || (year % 400 === 0)) {
-            result = `${year} год является високосным`
-        } else {
-            result = `${year} не високосный`;
-        }
-    } else {
-        result = "Вы не ввели число"
-    }
+// // TASK6
+// function isYearLeap() {
+//     let year = Number(prompt("Введите год"));
+//     let result;
+//     if (Number.isInteger(year)) {
+//         if ((year % 4 === 0 && !(year % 100 === 0)) || (year % 400 === 0)) {
+//             result = `${year} год является високосным`
+//         } else {
+//             result = `${year} не високосный`;
+//         }
+//     } else {
+//         result = "Вы не ввели число"
+//     }
         
-    console.log(result)
-}
+//     console.log(result)
+// }
 
-isYearLeap()
+// isYearLeap()
 
-//TASK 7
-function bank() {
-    let n = Number(prompt("Сколько рублей хотите вложить?"));
-    let year = Number(prompt("Сколько лет будете хранить?"));
-    if (Number.isInteger(n) && (Number.isInteger(year))) {
-        let i = 0;  
-        while (i < year) {
-            n = n + n * 0.1
-            i++
-        }
-        console.log(n)
-    } else {
-        console.log("Ошибка, вы не ввели числа")
-    }
-}
+// //TASK 7
+// function bank() {
+//     let n = Number(prompt("Сколько рублей хотите вложить?"));
+//     let year = Number(prompt("Сколько лет будете хранить?"));
+//     if (Number.isInteger(n) && (Number.isInteger(year))) {
+//         let i = 0;  
+//         while (i < year) {
+//             n = n + n * 0.1
+//             i++
+//         }
+//         console.log(n)
+//     } else {
+//         console.log("Ошибка, вы не ввели числа")
+//     }
+// }
 
-bank()
+// bank()
 
 // TASK8
 function area() {
@@ -173,29 +173,31 @@ function area() {
 
     let name = prompt("Выберите: прямоугольник, треугольник или круг")
     if (name === "прямоугольник") {
-        a = parseFloat(prompt("Чему равна его длина?"));
-        b = parseFloat(prompt("Чему равна его ширина?"));
-        if ((typeof a === 'number' && !Number.isNaN(a)) && (typeof b === 'number' && !Number.isNaN(b))) {
+        a = +prompt("Чему равна его длина?");
+        b = +prompt("Чему равна его ширина?");
+        if (a && b) {
             rectangle()
         } else {
             result = "Ошибка, вы не ввели числа"
         }
     } else if (name === "треугольник") {
-        a = parseFloat(prompt("Чему равна его длина?"));
-        b = parseFloat(prompt("Чему равна его ширина?"));
-        if ((typeof a === 'number' && !Number.isNaN(a)) && (typeof b === 'number' && !Number.isNaN(b))) {
+        a = +prompt("Чему равна его длина?");
+        b = +prompt("Чему равна его ширина?");
+        if (a && b) {
             triangle()
         } else {
             result = "Ошибка, вы не ввели числа"
         }
     } else if (name === "круг") {
-        r = parseFloat(prompt("Чему равен его радиус"))
-        if ( typeof r === 'number' && !Number.isNaN(r) ) {
+        r = +prompt("Чему равен его радиус")
+        if ( r ) {
             circle()
         } else {
             result = "Ошибка, вы не ввели числа"
         }
-    } 
+    } else {
+        console.log("Вы неверно ввели название фигуры")
+    }
 
 }
 
